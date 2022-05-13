c6480905a37547a9648246af9e396091f9116d96
Anthony Romrell <anthonyromrell@mac.com>
2022-05-13 15:14:59 -0600

    Examples

---
+++ Basics/001 Conventions and Syntax/ClassExample.cs
@@ -0,0 +1,15 @@
+public class ClassExample
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "Bob";
+}
+
+public class ClassWithFunctions
+{
+    
+}
