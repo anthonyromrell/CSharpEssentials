c4f2fb7dff9c8a68dede8ab805e0dc0986a8679d
Anthony Romrell <anthonyromrell@mac.com>
2022-05-18 13:24:01 -0600

    Updated MyClass

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -1,1 +1,6 @@
 //Based on the HelloWorld File, turn this into a class.
+
+//Write a console program with a program class, and Main Function
+//Add another class with any name, add variables 
+//Using the new keyword create several objects
+//Console writeline the Objects variables
