b7df4356d181d5d206c672b5ee9ad87cf0d382fd
Anthony Romrell <anthonyromrell@mac.com>
2022-05-18 13:17:38 -0600

    Added Hero Work

---
+++ Basics/001 Conventions and Syntax/NewHeroWork.cs
@@ -0,0 +1,30 @@
+using System;
+					
+public class Program
+{
+    public Hero heroOne;
+    public Hero heroTwo;
+	
+    public void Main()
+    {
+        heroOne = new Hero();
+        heroTwo = new Hero();
+		
+        heroOne.health = 2;
+        heroOne.powerLevel = 5;
+		
+        heroTwo.health = 3;
+        heroTwo.powerLevel = 1;
+		
+        Console.WriteLine(heroOne.health);
+        Console.WriteLine(heroOne.powerLevel);
+		
+        Console.WriteLine(heroTwo.health);
+        Console.WriteLine(heroTwo.powerLevel);
+    }
+}
+
+public class Hero {
+    public int health;
+    public int powerLevel;
+}//Based on the HelloWorld File, turn this into a class.
