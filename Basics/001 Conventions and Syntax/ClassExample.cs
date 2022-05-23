9a5a76f76ebae819f0186a678edd0de9ad4e7792
kaluek98 <105400174+kaluek98@users.noreply.github.com>
2022-05-23 17:11:32 -0600

    classfunctions

--- Basics/001 Conventions and Syntax/ClassExample.cs
+++ Basics/001 Conventions and Syntax/ClassExample.cs
@@ -1,21 +1,9 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
-
 public class ClassExample
 {
-    
-}
+    public int numberCountForHealth;
 
-public class ClassWithVariables
-{
-    public int number = 2;
-    public string userName = "Bob";
-}
-
-public class ClassWithFunctions
-{
-    public void RunFunction()
+    public void RunExample()
     {
-        Console.WriteLine("Running Function");
+        Console.Writeline("Hello World");
     }
-}   
+}
