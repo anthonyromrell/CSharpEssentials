70f930d0c1047383ca686e24f1b3b014a278e048
Anthony Romrell <anthonyromrell@mac.com>
2022-05-24 15:37:51 -0600

    Practice

---
+++ Basics/001 Conventions and Syntax/Lab1Practice.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Anthony";
+      Console.WriteLine(name); 
+      int number = 11;
+      Console.WriteLine(number);
+     }
+  }
+}
+
