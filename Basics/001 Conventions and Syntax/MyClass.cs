29b874f40f0bec2adea7bb9964f9e4d40c6a43f5
kaluek98 <105400174+kaluek98@users.noreply.github.com>
2022-05-27 20:09:03 -0600

    created my class

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -1,9 +1,54 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
+using System;
+					
+public class Program
+{
+	public Weapon wOne;
+	public Weapon wTwo;
+	public Weapon wThree;
+	
+	public void Main()
+	{
+		wOne = new Weapon();
+		wTwo = new Weapon();
+		wThree = new Weapon();
+		
+		wOne.Name = "MP5";
+		wOne.Power = 5;
+		wOne.Ammo = 30;
+		wOne.Range = 5;
+		
+		wTwo.Name = "RPG";
+		wTwo.Power = 25;
+		wTwo.Ammo = 4;
+		wTwo.Range = 8;
+		
+		wThree.Name = "1911";
+		wThree.Power = 7;
+		wThree.Ammo = 18;
+		wThree.Range = 7;
+		
+    Console.WriteLine("CHOOSE YOUR WEAPON");
+		Console.WriteLine(wOne.Name);
+		Console.WriteLine("Damage: " + wOne.Power);
+		Console.WriteLine("Capacity: " + wOne.Ammo);
+		Console.WriteLine("Range: " + wOne.Range + "00 meters");
+		
+		Console.WriteLine(wTwo.Name);
+		Console.WriteLine("Health: " + wTwo.Power);
+		Console.WriteLine("Capacity: " + wTwo.Ammo);
+		Console.WriteLine("Range: " + wTwo.Range + "00 meters");
+		
+		Console.WriteLine(wThree.Name);
+		Console.WriteLine("Health: " + wThree.Power);
+		Console.WriteLine("Capacity: " + wThree.Ammo);
+		Console.WriteLine("Range: " + wThree.Range + "00 meters");
+	}
+}
 
-//Based on the HelloWorld File, turn this into a class.
-
-//Write a console program with a program class, and Main Function
-//Add another class with any name, add variables 
-//Using the new keyword create several objects
-//Console writeline the Objects variables
+public class Weapon {
+	
+	public string Name;
+	public int Power;
+	public int Ammo;
+	public int Range;
+}
