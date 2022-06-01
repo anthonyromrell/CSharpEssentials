373ea33ea38ef605a1ebf1717531ad9629110578
Anthony Romrell <anthonyromrell@mac.com>
2022-05-31 18:46:02 -0600

    IF Examples

---
+++ Basics/002FunctionsAndConditionals/ConditionalsLab.cs
@@ -0,0 +1,43 @@
+using System;
+					
+public class Program
+{
+	public Operations myOperator;
+	
+	public void Main()
+	{
+		myOperator = new Operations();
+		
+		Console.WriteLine("Welcome");
+		myOperator.DoMath(10, 4);
+		myOperator.DoMath(20, 7);
+		myOperator.DoMath(30, 15);
+		myOperator.Compare(4,3);
+		myOperator.Compare(3,4);
+		myOperator.CheckPassword("SevenOF9");
+		myOperator.CheckPassword("OU812");
+	}
+}
+
+public class Operations {
+	public void DoMath (int value, int value2) {
+		var number = value + value2;
+		Console.WriteLine(number);
+	}
+	
+	public void Compare (int value, int value2) {
+		if(value > value2) {
+			Console.WriteLine("True, the first greater.");
+		} else {
+			Console.WriteLine("False, the second is greater.");
+		}
+	}
+	
+	public void CheckPassword (string password) {
+		if(password == "OU812") {
+			Console.WriteLine("Correct Password");
+		} else {
+			Console.WriteLine("Incorrect Password");	
+		}
+	}
+}
---
+++ Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
@@ -0,0 +1,4 @@
+// The Expectation is that you write your own code based
+// on what you have learned in the class, it can be what you want
+// If you are havind trouble thinging of something, write conditionals
+// with the topic of PowerUps, what happens if the powerUp is good or bad.
