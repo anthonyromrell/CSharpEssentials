59d12ae370f16fb4e21a2331ea939dc752490b6e
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-01-12 19:44:06 -0700

    Added Beam Sword info files

---
+++ Basics/001 Conventions and Syntax/Beam Sword.cs
@@ -0,0 +1,36 @@
+using System;
+					
+public class Program
+{
+	public MeleeWeapon beamSword;
+	public ChargeUp tBag;
+	
+	public void Main()
+	{
+		beamSword = new MeleeWeapon();
+		tBag = new ChargeUp();
+			
+		beamSword.basicDmg++;
+		beamSword.basicDmg += tBag.dmgLevel;
+		
+		Console.WriteLine(beamSword.meleeWeaponName + ". Halo's most iconic weapon, now in C#! ");
+		Console.WriteLine("Durability of " + beamSword.durability);
+		Console.WriteLine("Right click to stab for " + beamSword.basicStab + " damage!" );
+		Console.WriteLine("TBag for an increase of your basic attacks " +  "to " + beamSword.basicDmg + " instead of the usual 5.5.");
+		
+	
+	}
+}
+
+public class MeleeWeapon {
+	public int durability = 15;
+	public string meleeWeaponName = "Beam Sword";
+	public float basicDmg = 5.5f;
+	public int basicStab = 20;
+	
+}
+
+public class ChargeUp{
+		public float dmgLevel = 8.5f;
+	
+}
