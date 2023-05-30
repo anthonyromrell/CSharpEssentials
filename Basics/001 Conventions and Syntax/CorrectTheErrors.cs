0ffe6af1751e72fa98d24ae9208c426443499ae0
Strattiiix <133467582+Strattiiix@users.noreply.github.com>
2023-05-30 00:50:16 -0600

    Made my own code for each folder

--- Basics/001 Conventions and Syntax/CorrectTheErrors.cs
+++ Basics/001 Conventions and Syntax/CorrectTheErrors.cs
@@ -1,11 +1,11 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
-using Sytem;
+
+
+using System;
 
 public class CorrectTheErrors
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("HW);
-      
+        Console.WriteLine ("HW"); 
+} 
 }
--- Basics/001 Conventions and Syntax/HelloWorld.cs
+++ Basics/001 Conventions and Syntax/HelloWorld.cs
@@ -1,5 +1,4 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
+
 using System;
 
 public class HelloWorld
@@ -7,6 +6,6 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         //Change "HW" to "Hello World"
-        Console.WriteLine("HW");
+        Console.WriteLine("Hello World");
     }    
 }
