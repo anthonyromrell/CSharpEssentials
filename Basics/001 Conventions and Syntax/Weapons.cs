6c8fb74497082282565eb7975ab306a0116a7c7f
ShadeRenard <143022889+ShadeRenard@users.noreply.github.com>
2023-09-07 20:47:38 -0600

    changing name

--- Basics/001 Conventions and Syntax/Weapons.cs
+++ Basics/001 Conventions and Syntax/Weapons.cs
@@ -2,18 +2,18 @@ using System;
 					
 public class Program
 {
-	public Weapon miniGun;
+	public Weapon Laserbeam;
 	public PowerUp weaponPowerUp;
 	
 	public void Main()
 	{
-		miniGun = new Weapon();
+		Laserbeam = new Weapon();
 		weaponPowerUp = new PowerUp();
 		
-		miniGun.blowCount ++;
-		miniGun.freezeRay += weaponPowerUp.fluid;
-		Console.WriteLine(miniGun.weaponName + " has an ammo count of " + miniGun.blowCount + ".");
-		Console.WriteLine("The fluid in the freeze ray has gone below " + miniGun.freezeRay + ", it below freezing.");
+		Laserbeam.blowCount ++;
+		Laserbeam.freezeRay += weaponPowerUp.fluid;
+		Console.WriteLine(Laserbeam.weaponName + " has an ammo count of " + Laserbeam.blowCount + ".");
+		Console.WriteLine("The fluid in the freeze ray has gone below " + Laserbeam.freezeRay + ", it below freezing.");
 	}
 }
 
